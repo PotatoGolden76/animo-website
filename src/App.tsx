b3b368f3b29cc23d7f47eef81fc5fd6a290ab2a5
Harangus Robert <potatogolden76@gmail.com>
2022-09-22 10:55:27 +0300

    homepage V1 done

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,29 @@ import styles from './styles/homepage.module.scss';
 import classNames from 'classnames';
 
 import {
-  faCoffee,
-  faDice,
+  faBacterium,
   faEnvelope,
   faStar,
-  faGears
+  faFolderTree, faShrimp
 } from '@fortawesome/free-solid-svg-icons'
+import { TypeAnimation } from 'react-type-animation';
 
 export default function App() {
   const containerTStyle = "container mx-auto p-5 flex gap-8 flex-wrap flex-row justify-center items-center";
   return (
 
     <div className={classNames(containerTStyle, styles.homeContainer)}>
-      <h1>Hi, I'm Robert</h1>
-      <PowerCard text="About" destination="/about" color="red" icon={faCoffee} />
-      <PowerCard text="Projects" destination="/projects" color="orange" icon={faGears} />
-      <PowerCard text="Experience" destination="/experience" color="yellow" icon={faStar} />
+      <TypeAnimation
+        sequence={["< Hi, I'm Robert />"]}
+        wrapper="h1"
+        cursor={true}
+        repeat={0}
+        style={{ fontSize: '4rem' }}
+      />
+      <PowerCard text="About" destination="/about" color="red" icon={faBacterium} />
+      <PowerCard text="Projects" destination="/projects" color="orange" icon={faFolderTree} />
+      <PowerCard text="Resume" destination="/resume" color="yellow" icon={faStar} />
       <PowerCard text="Contact" destination="/contact" color="green" icon={faEnvelope} />
-      <PowerCard text="Random" destination="/contact" color="blue" icon={faDice} />
     </div>
 
   );
